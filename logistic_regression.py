8529b9408884fb517d6cbe13a34a31a8dcbdf595
Eliot Solomon <ehs3@rice.edu>
2020-01-25 00:30:56 -0600

    Started working on gradient descent.

--- logistic_regression.py
+++ logistic_regression.py
@@ -22,3 +22,32 @@ with open('divorce.csv', newline='') as csv_file:
 # Numpy matrices
 x_values = np.matrix(raw_x_values)
 y_values = np.matrix(raw_y_values)
+
+n = 54
+m = 170
+
+def linear_model(weights, x):
+    """
+    Evaluate the linear component of the logistic model given a set of weights and x values.
+    """
+    return np.dot(weights, x)
+
+def sigmoid_model(z):
+    """
+    Evaluate the sigmoid function with input value z.
+    """
+    return 1 / (1 + exp(z))
+
+def calculate_gradient_vector(weights, x_matrix, y_vector):
+    """
+    Calculate the gradient vector given a set of current weights, as well as the training data.
+    """
+    gradient_vector = np.ones(n)
+    constant_vector = np.ones(m)
+    for i in range(m):
+        constant_vector[i] = sigmoid_model(linear_model(weights, x_matrix[i])) - y_vector[i, 0] # index?
+    for j in range(n):
+        gradient_vector[j] = np.dot(constant_vector, np.transpose(x_matrix[:,j]))
+
+
+
