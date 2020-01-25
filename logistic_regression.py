fdbadd946e6e5ba28ed8ee76b0c88c8738a45222
Eliot Solomon <ehs3@rice.edu>
2020-01-25 10:47:13 -0600

    Clean up code, and test.

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,12 +9,8 @@ raw_y_values = []
 with open('divorce.csv', newline='') as csv_file:
     divorce_data = csv.reader(csv_file)
 
-    # Header bool used to skip the header row in CSV file
-    header = True
+    next(divorce_data) # skip header row
     for row in divorce_data:
-        if header:
-            header = False
-            continue
         row = row[0].split(";")
         x_values = list(map(float, row[:-1]))
         y_values = list(map(float, row[-1:]))
@@ -24,9 +20,6 @@ with open('divorce.csv', newline='') as csv_file:
 # Numpy matrices
 x_values = np.matrix(raw_x_values)
 y_values = np.matrix(raw_y_values)
-
-n = 54
-m = 170
 
 def linear_model(weights, x):
     """
@@ -44,10 +37,11 @@ def calculate_gradient_vector(weights, x_matrix, y_vector):
     """
     Calculate the gradient vector given a set of current weights, as well as the training data.
     """
+    m, n = x_values.shape
     gradient_vector = np.ones(n)
     constant_vector = np.ones(m)
     for i in range(m):
-        constant_vector[i] = sigmoid_model(linear_model(weights, x_matrix[i].A1)) - y_vector[i, 0] # index?
+        constant_vector[i] = sigmoid_model(linear_model(weights, x_matrix[i].A1)) - y_vector[i, 0]
     for j in range(n):
         gradient_vector[j] = np.dot(constant_vector, np.transpose(x_matrix[:,j]).A1)
     return gradient_vector
@@ -56,6 +50,7 @@ def logistic_gradient_descent(x_matrix, y_vector, learning_rate, threshold):
     """
     Perform logistic regression via the gradient descent algorithm.
     """
+    n = x_values.shape[1]
     weights = np.ones(n)
     while True:
         new_weights = weights - learning_rate * calculate_gradient_vector(weights, x_matrix, y_vector)
@@ -63,6 +58,18 @@ def logistic_gradient_descent(x_matrix, y_vector, learning_rate, threshold):
             return new_weights
         weights = new_weights
 
-output = logistic_gradient_descent(x_values, y_values, 0.1, 0.001)
+output = logistic_gradient_descent(x_values, y_values, 0.01, 0.001)
 
 print(output.tolist())
+
+test = np.array([2] * 54)
+
+print(sigmoid_model(linear_model(output, test)))
+
+test2 = x_values[160].A1
+
+print(sigmoid_model(linear_model(output, test2)))
+
+test3 = x_values[40].A1
+
+print(sigmoid_model(linear_model(output, test3)))
